b8ec7f1e408d38736e63c894b428e4c4aac3d3e2
ayushmaan <ayushmaanroy.12@gmail.com>
2023-02-13 13:52:41 +0530

    display structure created

--- src/component/App.js
+++ src/component/App.js
@@ -1,21 +1,77 @@
 import React from 'react';
 
-
-class App extends'React.component'{
-  constructor(){
+class App extends 'React.component'{
+  constructor() {
     super();
     this.state = {
-      
+      active: 0,  //active list items
+      menuItems: ["now Playing", "Music", "Games", "settings"],    //menu items
+      musicItems: ["All songs", "Artists", "Album"],            //music items
+      songItemsUrl: [song1, song2, song3, song4],              //song list
+      songImgItemsUrl: [song1Img, song2Img, song3Img, song4Img],  //song img list
+
+      wallpaperItems: [wallpaper1, wallpaper2, wallpaper3],       //list of wallpaper
+      songItems: [],                                             //list of song names
+      songIndex: 0,                                                //current song
+      lengthMenuKey: { "-1": 3, 1: 2, 4: 4, 8: 4, 3: 2, 9: 3, 10: 2 },  //length of a particular menu 
+
+      menuMapping: { "-1": [0, 1, 2, 3], 1: [4, 5, 6], 3: [8, 9, 10] }, //which menu can be rendered by key menu
+      currentMenu: -2,                                           //current menu which is lockscreen initially
+      navigationStack: [],                                     //used for navigation forward and backward
+      songUrl: song1,                                              //current song url
+      playing: false,                                             //playing or not 
+      theme: "rgb(210,210,210)",
+      audio: new Audio(song1),                                  //current audio file
+      songImgUrl: song1Img,                                     //current song img for now playing
+      wheelColor: "white",                                      //current wheel color
+      wallpaper: 0,                                             //current wallpaper
+      noty: false,                                               // has to show notification or not
+      notifyText: "Wallpaper Changed",                           //notification text
     }
   }
+
+
+
+
+
+  // FUNCTION FOR : RENDERING APP
   render() {
+    const { audio, active, currentMenu, menuItems, musicItems, songItems, playing, songIndex, theme, songUrl, songImgUrl, wheelColor, wallpaper, wallpaperItems, noty, notifyText } = this.state;
     return (
-     <div className='App'>
-        
-     </div>
+      <div className='App'>
+        <Case
+          songIndex={songIndex}
+          active={active}
+          menuItems={menuItems}
+          currentMenu={currentMenu}
+          musicItems={musicItems}
+          theme={theme}
+          songUrl={songUrl}
+          songImgUrl={songImgUrl}
+          audio={audio}
+          songItems={songItems}
+          playing={playing}
+          wheelColor={wheelColor}
+          wallpaper={wallpaper}
+          wallpaperItems={wallpaperItems}
+
+          changeMenuForward={this.changeMenuForward}
+          changeMenuBackward={this.changeMenuBackward}
+          updateActiveMenu={this.updateActiveMenu}
+          togglePlayPause={this.togglePlayPause}
+          seekSongForward={this.seekSongForward}
+          seekSongReverse={seekSongReverse}
+          noty={noty}
+          setNoty={this.setNoty}
+          notifyText={this.notifyText}
+
+        />
+
+
+      </div>
     );
   }
- 
+
 }
 
 export default App;
---
+++ src/component/Case.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Display from './Display';
+import Wheel from './Wheel';
+
+
+// This component is the outer case of iPod it does nothing special just renders display and wheel component
+class Case extends React.Component {
+    render() {
+        const { active, updateActiveMenu, currentMenu, changeMenuBackward, changeMenuForward, menuItems, musicItems, togglePlayPause, songItems, playing, songIndex, theme, audio, songUrl, songImgUrl, seekSongForward, seekSongReverse, wheelColor, wallpaper, wallpaperItems, noty, setNoty, notifyText } = this.props;
+
+        return (
+            <div className="case-container">
+                <div style={{ backgroundColor: theme }} className="case">
+                    <Display
+                        songIndex={songIndex}
+                        playing={playing}
+                        noty={noty}
+                        setNoty={setNoty}
+                        notifyText={notifyText}
+                        musicItems={musicItems}
+                        songItems={songItems}
+                        songUrl={songUrl}
+                        songImgUrl={songImgUrl}
+                        audio={audio}
+                        wallpaper={wallpaper}
+                        wallpaperItems={wallpaperItems}
+                    />
+                    <Wheel
+                        updateActiveMenu={updateActiveMenu}
+                        changeMenuBackward={changeMenuBackward}
+                        togglePlayPause={togglePlayPause}
+                        changeMenuForward={changeMenuForward}
+                        currentMenu={currentMenu}
+                        menuItems={menuItems}
+                        seekSongForward={seekSongForward}
+                        seekSongReverse={seekSongReverse}
+                        active={active}
+                        wheelColor={wheelColor}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+export default Case;
